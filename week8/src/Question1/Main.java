34a5f94fffd6844db42a1caf1383028bfcd6ad84
1josee <trnnhattien@gmail.com>
2022-12-13 15:36:29 +0700

    add

--- week8/src/Question1/Main.java
+++ week8/src/Question1/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String name, adresses, fileName;
         int age;
-        System.out.println("Enter the name of the file:");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the file:");
         fileName = scanner.nextLine();
         PrintWriter pw = new PrintWriter(new FileWriter(fileName,true));
 
@@ -21,6 +21,7 @@ public class Main {
         System.out.println("Enter age: ");
         age = scanner.nextInt();
         pw.println(name+ "," + adresses + "," + age);
+        pw.flush();
         pw.close();
     }
 }
--- week8/src/Question2/Main.java
+++ week8/src/Question2/Main.java
@@ -3,13 +3,24 @@ package Question2;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner fileScanner = new Scanner(new File("scr/users.txt"));
-
-        While(fileScanner.hasNext()){
-            System.out.println();
+        String name, adresses, age;
+        ing age;
+        Scanner fileScanner = new Scanner(new File("/week8/users.txt"));
+        while (fileScanner.hasNext()){
+            line = fileScanner.nextLine();
+            StringTokenizer inReader = new StringTokenizer(line,",");
+            if(inReader.countTokens() != 3){
+                throw new IOException("invalid input");
+            } else {
+                name = inReader.nextToken;
+                adresses =inReader.nextToken;
+                age = Interger.parseInt(inReader.nextToken());
+            }
+            System.out.println(name +","+ adresses +","+ age);
         }
     }
 }
